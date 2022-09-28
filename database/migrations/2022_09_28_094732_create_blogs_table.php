15bd0b6c9d917ee3c504ac881065c62c78ad82da
Sandip Nandy <sandeepnandi4@gmail.com>
2022-09-28 16:19:21 +0530

    Blogs, tags, categories, images - models and migrations

---
+++ database/migrations/2022_09_28_094732_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->integer('category_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->enum('status', [0, 1])->nullable()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
